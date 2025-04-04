f19ae1ab39485aa35be0db24c1620cd9faef65ea
mauryavinay1407 <vinaymaurya1407@gmail.com>
2025-04-04 23:34:28 +0530

    feat: home page complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import Captainlogin from "./pages/Captainlogin";
+import CaptainSignup from "./pages/CaptainSignup";
 
 function App() {
   return (
-    <>
-      <h1 className = "text-6xl text-red-600">
-         Hello from client
-      </h1>
-    </>
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<UserLogin/>} />
+        <Route path="/signup" element={<UserSignup/>} />
+        <Route path="/captain-login" element={<Captainlogin/>} />
+        <Route path="/captain-signup" element={<CaptainSignup/>} />
+      </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='bg-cover bg-center bg-[url("bg.png")] h-screen  pt-4 w-full flex items-center justify-between flex-col bg-red-400'>
+          <div className='w-full'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" className='w-16 ml-8'/>
+          </div>
+            <div className=' bg-white py-4 px-4 w-full sm:w-2/4'>
+                <h2 className='text-3xl font-bold'>Get Started with Uberz</h2>
+                <Link to="/login" className='w-full flex items-center justify-center bg-black text-white py-3 rounded mt-5'>Continue</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home;
